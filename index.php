ab1f9d0bdfa395d625f4d178470791b49a708929
sreeram4bigdata <47217651+sreeram4bigdata@users.noreply.github.com>
2020-01-08 13:50:58 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+echo "Test Page PHP";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Fusion Charts Column 2D Sample</title>
+</head>
+<body>
+  <div id="chart-container">FusionCharts will render here</div>
+ 
+</body>
+</html>
